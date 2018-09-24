fefd911acdbeae479434a636fe9067c79fe1c3f1
Scott Randolph Edmonds <info@scottedmonds.com>
2018-09-24 16:33:38 -0400

    Test: create AddressBook constructor

---
+++ src/AddressBook.js
@@ -0,0 +1,8 @@
+// Get our test to turn green for *AddressBook is not defined*
+
+// 1. create AddressBook constructor function
+function AddressBook() {
+
+}
+
+// Expected: "ReferenceError: Contact is not defined"
